84055bd348873770cff807bfefe8c89b11ffed10
jacksonhales <me@jacksonhales.com.au>
2017-11-03 14:28:16 +1100

    Controller v1 Commit

--- LegoArena/LegoArena.ClassLibrary/Controller.cs
+++ LegoArena/LegoArena.ClassLibrary/Controller.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lego;
+using Lego.Ev3;
+using Lego.Ev3.Desktop;
 
 namespace LegoArena.ClassLibrary
-{
+{       
+    // JACKSONS CODE
+
     class Controller
     {
+        Brick brick;
+        GyroSensor gyroSensor;
+        ColourSensor colourSensor;
+        UltrasonicSensor ultrasonicSensor;
+        Movement movement;
+        Motor motor;
+
+        public Controller()
+        {
+            brick = new Brick();
+            gyroSensor = new GyroSensor();
+            colourSensor = new ColourSensor();
+            ultrasonicSensor = new UltrasonicSensor();
+            movement = new Movement();
+            motor = new Motor();
+        }
     }
 }
