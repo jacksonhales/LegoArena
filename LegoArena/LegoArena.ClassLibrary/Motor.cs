bde7a9c257beb662dd27efe1aacc0c9029ccc397
Dylan Peterson <101408728@student.swin.edu.au>
2017-11-03 16:02:46 +1100

    Changed functions from turning specific directions to more flexible ways that can be changed as we go.

--- LegoArena/LegoArena.ClassLibrary/Motor.cs
+++ LegoArena/LegoArena.ClassLibrary/Motor.cs
@@ -4,28 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lego.Ev3.Desktop;
-
+using Lego;
+using Lego.Ev3;
+using Lego.Ev3.Core;
 namespace LegoArena.ClassLibrary
 {
     //Dylan's Code
     public class Motor
     {
-        public void MoveLeft()
+        public async void TurnMotorAtPowerAsync(OutputPort outPort, int power)
         {
-           
+            await Controller.TeamBrick.Brick.DirectCommand.TurnMotorAtPowerAsync(outPort, power);
         }
-        public void MoveRight()
+
+        public async void TurnMotorAtSpeedAsync(OutputPort outPort, int speed)
+        {
+            await Controller.TeamBrick.Brick.DirectCommand.TurnMotorAtSpeedAsync(outPort, speed);
+        }
+        
+        public async void TurnMotorAtPowerForTimeAsync(OutputPort outPort, int power, uint time, bool Bool)
+        {
+            await Controller.TeamBrick.Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(outPort, power, time, Bool);
+        }
+
+        public async void TurnMotorAtSpeedForTimeAsync(OutputPort outPort, int speed, uint time, bool Bool)
+        {
+            await Controller.TeamBrick.Brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(outPort, speed, time, Bool);
+        }
+        /*public async void Brick_BrickChanged(object sender, BrickChangedEventArgs e)
         {
 
-        }
-        public void TurnBackward()
-        {
-
-        }
-        public void TurnForward()
-        {
-
-        }
+        }*/
     }
     
 
